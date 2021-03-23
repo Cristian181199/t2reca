2bb97b8faedb936fa0a06008a197915491440ebd
Cristian Quintero Garcia <cristian181199@gmail.com>
2021-03-23 15:57:04 +0100

    Modificacion tonta

--- migrations/m210323_143755_create_reservas_table.php
+++ migrations/m210323_143755_create_reservas_table.php
@@ -16,9 +16,9 @@ class m210323_143755_create_reservas_table extends Migration
             'id' => $this->bigPrimaryKey(),
             'usuario_id' => $this->bigInteger()->notNull(),
             'vuelo_id' => $this->bigInteger()->notNull(),
-            'asiento' => $this->integer()->notNull()->check('asiento >= 0'),
-            'UNIQUE (vuelo_id, asiento)',
-            'UNIQUE (usuario_id, vuelo_id)',
+            'asiento' => $this->integer()->notNull()->check('asiento > 0'), // Comprobacion de que el asiento es mayor a 0
+            'UNIQUE (vuelo_id, asiento)', // No puede haber un asiento reservado dos veces en el mismo vuelo.
+            'UNIQUE (usuario_id, vuelo_id)', // Un usuario no puede reservar un mismo vuelo 2 veces.
         ]);
     }
 
